f90cb36283f22386da55ba25a6f84ed354124223
programmingAthlete <luca.bonamino@hotmail.com>
2025-01-27 02:29:30 +0100

    put back parallelization

--- src/restricted_algebraic_immunity/inductive_reed_muller/IV.py
+++ src/restricted_algebraic_immunity/inductive_reed_muller/IV.py
@@ -464,13 +464,13 @@ class IVRestrictedAI:
                 (z, z_c, e, s_bin),
                 (z_c, z, e, s_bin)
             ]
-            imm1 = f_ummu.find_min_annihilator(z, z_c, e, s_bin)
+            # imm1 = f_ummu.find_min_annihilator(z, z_c, e, s_bin)
 
-            imm2 = f_ummu.find_min_annihilator(z_c, z, e, s_bin)
+            # imm2 = f_ummu.find_min_annihilator(z_c, z, e, s_bin)
 
-            results = [imm1, imm2]
-            # with Pool(processes=2) as pool:
-            #     results = pool.starmap(f_ummu.find_min_annihilator, args)
+            # results = [imm1, imm2]
+            with Pool(processes=2) as pool:
+                 results = pool.starmap(f_ummu.find_min_annihilator, args)
             return min([item for item in results if item is not None])
         else:
             return f_ummu.fin_min_annihilator_sequencial(z=z, z_c=z_c, e=e, s=s_bin)
