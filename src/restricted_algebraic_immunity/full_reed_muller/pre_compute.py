fb9387fcc842834b06fb22a2fef6555aa79040f5
programmingAthlete <luca.bonamino@hotmail.com>
2025-02-18 22:16:56 +0100

    pre-compute

--- src/restricted_algebraic_immunity/full_reed_muller/pre_compute.py
+++ src/restricted_algebraic_immunity/full_reed_muller/pre_compute.py
@@ -30,8 +30,8 @@ def pre_compute_all(n_vars):
     m = pre_compute_all_matrices(n=n_vars, restricted=True)
     degrees = pre_compute_degrees(n=n_vars, restricted=True)
     dt = time.time() - t
-    save_to_matrices_file(m)
-    save_deg_to_file(degrees)
+    save_to_matrices_file(m, n_vars)
+    save_deg_to_file(degrees, n_vars)
     return dt
 
 def pre_compute_all_separated(n_vars):
@@ -41,17 +41,17 @@ def pre_compute_all_separated(n_vars):
     t = time.time()
     degrees = pre_compute_degrees(n=n_vars, restricted=True)
     dt_d = time.time() - t
-    save_to_matrices_file(m)
+    save_to_matrices_file(m, n_vars)
     save_deg_to_file(degrees)
     return dt_m, dt_d
 
 
-def save_deg_to_file(d: Dict):
-    save(d, f'{settings.root_path}/full_reed_muller/pre_computation/degs.sobj')
+def save_deg_to_file(d: Dict, n):
+    save(d, f'{settings.root_path}/full_reed_muller/pre_computation/degs_n_max_{n}.sobj')
 
 
-def save_to_matrices_file(d: Dict[int, List]):
-    save(d, f'{settings.root_path}/full_reed_muller/pre_computation/RMs.sobj')
+def save_to_matrices_file(d: Dict[int, List], n: int):
+    save(d, f'{settings.root_path}/full_reed_muller/pre_computation/RMs_n_max_{n}.sobj')
 
 
 def pre_compute_by_n_vars(n: int):
@@ -59,3 +59,6 @@ def pre_compute_by_n_vars(n: int):
     deg = {j: sum([binomial(n, k) for k in range(j + 1)]) for j in range(n + 1)}
     save({n: mat}, f'{settings.root_path}/full_reed_muller/pre_computation/RMs_{n}.sobj')
     save({n: deg}, f'{settings.root_path}/full_reed_muller/pre_computation/degs_{n}.sobj')
+
+def pre_compute_by_max_n_vars(n: int):
+    pre_compute_all(n)
