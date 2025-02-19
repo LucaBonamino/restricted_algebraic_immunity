836b871965e8f3077d7a486e917cb5ac689f39cf
Luca <44218525+programmingAthlete@users.noreply.github.com>
2025-02-19 22:09:40 +0100

    Ai9 (#13)
    * push
    
    * data

--- src/restricted_algebraic_immunity/factory/AIk_of_published_functions.py
+++ src/restricted_algebraic_immunity/factory/AIk_of_published_functions.py
@@ -1,11 +1,10 @@
 import argparse
 import enum
 import math
-
 from restricted_algebraic_immunity.boolean_functions.boolean_function import CustomBooleanFunction
 from restricted_algebraic_immunity.boolean_functions.known_boolean_functions.CMR import CMR
 from restricted_algebraic_immunity.boolean_functions.known_boolean_functions.DM24 import SubDM24, DM24
-from restricted_algebraic_immunity.boolean_functions.known_boolean_functions.TL import LT_WPB_rand
+from restricted_algebraic_immunity.boolean_functions.known_boolean_functions.TL import LT_WPB_rand, tl_truth_table
 from restricted_algebraic_immunity.boolean_functions.known_boolean_functions.ZS23 import ZS23
 from restricted_algebraic_immunity.entities.enums import ReturnType
 from restricted_algebraic_immunity.utils.utils import is_WPB
@@ -36,13 +35,18 @@ def zn_23(n: int = 16, check: bool = False):
     return df
 
 
-def tl(n: int, check: bool = False):
-    bf = LT_WPB_rand(n)
+def tl(n: int, random: bool = False, check: bool = False):
+    # if random is True:
+    if True:
+        bf = LT_WPB_rand(n)
+    else:
+        bf = tl_truth_table[n]
     print(bf)
     if check is True:
         assert is_WPB(bf)
     immu = CustomBooleanFunction.immunity_f_k(f=bf, n=n, return_type=ReturnType.DATA_FRAME)
     print(immu)
+    print(bf.truth_table(format='int'))
     return immu
 
 
@@ -59,15 +63,16 @@ def cmr(n: int, check: bool = False):
 
 class FunctionsInputEnum(enum.Enum):
     DM24 = "DM24"
-    ZS23 = "ZS23"
+    ZJZQ23 = "ZJZQ23"
     TL19 = "TL29"
-    CMR = "CMR"
+    CMR17 = "CMR17"
+
 
 class FunctionsEnum(enum.Enum):
     DM24 = ('DM24', dm_24)
-    ZS23 = ("ZS23", zn_23)
+    ZJZQ23 = ("ZJZQ23", zn_23)
     TL19 = ("TL29", tl)
-    CMR = ("CMR", cmr)
+    CMR17 = ("CMR17", cmr)
 
 
 if __name__ == '__main__':
@@ -76,5 +81,9 @@ if __name__ == '__main__':
                         help="Function family of the function of which to calculate the AIk", required=True)
     parser.add_argument('-n', '--n_vars', type=int, help='Number of variables of the function', required=True)
     parser.add_argument('-c', '--check', action='store_false', help="Check if the function is WPB.")
+    parser.add_argument('-r', '--random', action='store_false',
+                        help="Take a random function from the family - only usable with LT19")
     args = parser.parse_args()
+    if args.random is True and args.function != FunctionsInputEnum.TL19:
+        print(f"invalid --random argument for function {args.function.value}, ignoring the -r argument")
     print(FunctionsEnum[args.function.name].value[1](n=args.n_vars))
