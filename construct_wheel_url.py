02167bb1127b3269858defb2a6b915d8f6a2d8ca
programmingAthlete <luca.bonamino@hotmail.com>
2025-02-24 11:54:06 +0100

    base path

--- construct_wheel_url.py
+++ construct_wheel_url.py
@@ -3,26 +3,27 @@ import sys
 import os
 
 def get_wheel_url():
-    base_url = "https://github.com/programmingAthlete/alegebraic_immunity_utils/releases/download/1.4.1/"
+    version = '0.1.0'
+    base_url = f"https://github.com/LucaBonamino/restricted_algebraic_immunity/releases/download/{version}/"
     python_version = f"cp{sys.version_info.major}{sys.version_info.minor}"
     system = platform.system()
     architecture = platform.machine()
 
     if system == "Linux":
         if architecture == "x86_64":
-            wheel_file = f"algebraic_immunity_utils-1.4.1-{python_version}-{python_version}-manylinux_2_17_x86_64.manylinux2014_x86_64.whl"
+            wheel_file = f"algebraic_immunity_utils-{version}-{python_version}-{python_version}-manylinux_2_17_x86_64.manylinux2014_x86_64.whl"
         elif architecture == "aarch64":
-            wheel_file = f"algebraic_immunity_utils-1.4.1-{python_version}-{python_version}-manylinux_2_17_aarch64.manylinux2014_aarch64.whl"
+            wheel_file = f"algebraic_immunity_utils-{version}-{python_version}-{python_version}-manylinux_2_17_aarch64.manylinux2014_aarch64.whl"
     elif system == "Windows":
         if architecture == "AMD64":
-            wheel_file = f"algebraic_immunity_utils-1.4.1-{python_version}-{python_version}-win_amd64.whl"
+            wheel_file = f"algebraic_immunity_utils-{version}-{python_version}-{python_version}-win_amd64.whl"
         elif architecture == "x86":
-            wheel_file = f"algebraic_immunity_utils-1.4.1-{python_version}-{python_version}-win32.whl"
+            wheel_file = f"algebraic_immunity_utils-{version}-{python_version}-{python_version}-win32.whl"
     elif system == "Darwin":
         if architecture == "x86_64":
-            wheel_file = f"algebraic_immunity_utils-1.4.1-{python_version}-{python_version}-macosx_10_9_x86_64.whl"
+            wheel_file = f"algebraic_immunity_utils-{version}-{python_version}-{python_version}-macosx_10_9_x86_64.whl"
         elif architecture == "arm64":
-            wheel_file = f"algebraic_immunity_utils-1.4.1-{python_version}-{python_version}-macosx_11_0_arm64.whl"
+            wheel_file = f"algebraic_immunity_utils-{version}-{python_version}-{python_version}-macosx_11_0_arm64.whl"
     else:
         raise Exception(f"Unsupported platform: {system} {architecture}")
 
